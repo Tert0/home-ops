49433cfcc846d321a9e082fa2918ca424d309ac3
Tert0 <62036464+Tert0@users.noreply.github.com>
2023-03-08 16:24:36 +0100

    added metallb and sealed secrets

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Check if sealed secrets main.key exists
+if [[ ! -f main.key ]]
+then
+    echo "The sealed secrets main private key file (main.key) is missing."
+    exit 1
+fi
+
 # Check if Cluster is viable
 flux check --pre
 
@@ -8,6 +15,8 @@ kubectl apply -k kubernetes/bootstrap
 sleep 10
 # TODO automate with kubectl wait
 read -p "Press enter when the basic flux components are read."
+# Add sealed secret main.key
+kubectl 
 # Adding Cluster-wide ConfigMap
 kubectl apply -f kubernetes/flux/vars/cluster-settings.yaml
 # Adding Flux Configs (Repository and main Kustomization)
